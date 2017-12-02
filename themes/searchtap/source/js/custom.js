3b8cbf05344f60361e3aa4b795cd9a42246975cf
naveensky <naveensky@gmail.com>
2017-12-02 13:52:41 +0530

    Updated coming soon and contact us form netlify

--- themes/searchtap/source/js/custom.js
+++ themes/searchtap/source/js/custom.js
@@ -49,7 +49,7 @@ $(function () {
 
       }
     )
-  })
+  });
 
   $("#mainLoginForm").submit(function (event) {
     event.preventDefault();
@@ -82,10 +82,9 @@ $(function () {
         success: function (data) {
           window.location.href = "https://app.searchtap.io/users/sites/urls"
         }
-
       }
-    )
-  })
+    );
+  });
 
   $(document).ready(function () {
     var email = sessionStorage.getItem("emailToStart")
@@ -135,136 +134,73 @@ $(function () {
       .always(function () {
         $("#requestDemoFormSubmit").prop('disabled', false);
       });
-
-    // phone = "(" + $('.selected-dial-code').text() + ") " + phone;
-
-    // var obj = {};
-    // obj.properties = [];
-    //
-    // var property = function (property, value) {
-    //   this.property = property;
-    //   this.value = value;
-    // };
-    //
-    // obj.properties.push(new property("firstname", name));
-    // obj.properties.push(new property("phone", phone));
-    // obj.properties.push(new property("source", "Demo form"));
-    //
-
-
-    // $.ajax({
-    //     url: 'https://app.searchtap.io/crm/hubspot?email=' + email,
-    //     contentType: 'application/json',
-    //     type: 'POST',
-    //     data: JSON.stringify(obj),
-    //     xhrFields: {
-    //       withCredentials: true
-    //     },
-    //     success: function (data) {
-    //       $("#leadNotCreated").hide();
-    //       $("#input-hide").hide();
-    //       $("#leadCreated").show();
-    //       $("#requestDemoFormSubmit").prop('disabled', false);
-    //
-    //     },
-    //     error: function (err) {
-    //       console.log(err)
-    //       $("#requestDemoFormSubmit").prop('disabled', false);
-    //       $("#leadCreated").hide();
-    //       $("#leadNotCreated").show();
-    //     }
-    //
-    //   });
   });
 
   $("#contactUsForm").submit(function (event) {
     event.preventDefault();
+
+
     $("#contactUsFormSubmit").prop('disabled', true);
     var name = $(this).find("#name").val();
     var message = $(this).find("#message").val();
     var email = $(this).find("#email").val();
     var phone = $(this).find("#number").val();
+    phone = "(" + $('.selected-dial-code').text() + ") " + phone;
 
-    phone = "(" + $('.selected-dial-code').text() + ") " + phone
 
-    var obj = {};
-    obj.properties = [];
-
-    var property = function (property, value) {
-      this.property = property;
-      this.value = value;
+    var fd = {
+      'form-name': "Contact Us",
+      name: name,
+      message: message,
+      email: email,
+      phone: phone
     };
 
-    obj.properties.push(new property("firstname", name));
-    obj.properties.push(new property("message", message));
-    obj.properties.push(new property("phone", phone));
-    obj.properties.push(new property("source", "Website Contact form"));
-
-    $.ajax({
-        url: 'https://app.searchtap.io/crm/hubspot?email=' + email,
-        contentType: 'application/json',
-        type: 'POST',
-        data: JSON.stringify(obj),
-        xhrFields: {
-          withCredentials: true
-        },
-        success: function (data) {
-          $("#contactQueryNotCreated").hide();
-          $("#input-hide").hide();
-          $("#contactQueryCreated").show();
-          $("#contactUsFormSubmit").prop('disabled', false);
-
-        },
-        error: function (err) {
-          console.log(err)
-          $("#contactUsFormSubmit").prop('disabled', false);
-          $("#contactQueryCreated").hide();
-          $("#contactQueryNotCreated").show();
-        }
-
-      }
-    )
-  })
+    var $form = $(this);
+    $.post($form.attr("action"), $.param(fd))
+      .done(function () {
+        $("#contactQueryNotCreated").hide();
+        $("#input-hide").hide();
+        $("#contactQueryCreated").show();
+        $form.trigger('reset');
+      })
+      .fail(function (err) {
+        console.error(err);
+        $("#contactUsFormSubmit").prop('disabled', false);
+        $("#contactQueryCreated").hide();
+      })
+      .always(function () {
+        $("#contactUsFormSubmit").prop('disabled', false);
+      });
+  });
 
   $("#comingSoonForm").submit(function (event) {
     event.preventDefault();
     $("#comingSoonFormSubmit").prop('disabled', true);
+
     var email = $(this).find("#email").val();
 
-    var obj = {};
-    obj.properties = [];
-
-    var property = function (property, value) {
-      this.property = property;
-      this.value = value;
+    var fd = {
+      email: email,
+      "form-name": "Coming Soon"
     };
 
-    obj.properties.push(new property("source", "Coming Soon form"));
-
-    $.ajax({
-        url: 'https://app.searchtap.io/crm/hubspot?email=' + email,
-        contentType: 'application/json',
-        type: 'POST',
-        data: JSON.stringify(obj),
-        xhrFields: {
-          withCredentials: true
-        },
-        success: function (data) {
-          $("#notificationNotCreated").hide();
-          $("#input-hide").hide();
-          $("#notificationCreated").show();
-          $("#comingSoonFormSubmit").prop('disabled', false);
-
-        },
-        error: function (err) {
-          console.log(err)
-          $("#comingSoonFormSubmit").prop('disabled', false);
-          $("#notificationCreated").hide();
-          $("#notificationNotCreated").show();
-        }
-
-      }
-    )
+    var $form = $(this);
+    $.post($form.attr("action"), $.param(fd))
+      .done(function () {
+        $("#notificationNotCreated").hide();
+        $("#input-hide").hide();
+        $("#notificationCreated").show();
+        $form.trigger('reset');
+      })
+      .fail(function (err) {
+        console.error(err);
+        $("#notificationCreated").hide();
+        $("#notificationNotCreated").show();
+      })
+      .always(function () {
+        $("#comingSoonFormSubmit").prop('disabled', false);
+      });
   })
 
 });
